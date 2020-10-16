f4d48e3093b7d29c4625fe5c20a91ef216bb1d3d
kasia.kordys <kasia.kordys@gmail.com>
2020-10-16 19:25:50 +0100

    add home controller

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "This is an index action on the home controller";
+        }
+    }
+}

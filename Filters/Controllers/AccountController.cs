0c4db9437a72a9d1f7c54aedc52139f8ac236159
kasia.kordys <kasia.kordys@gmail.com>
2020-10-16 19:54:14 +0100

    add account controller and login view

---
+++ Filters/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Filters.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+
+        //called for unauthorised access
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password, string returnUrl)
+        {
+            bool result = FormsAuthentication.Authenticate(username, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+            }
+            else
+            {
+                ModelState.AddModelError(" ", "username or passwor id invalid" );
+                return View();
+            }
+        }
+    }
+}
